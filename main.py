08a2e4ec755e29d48c3070b96b6cc81a06274fde
glory9 <53071086+glory9@users.noreply.github.com>
2019-11-09 05:56:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
+# Import the required module for text
+# to speech conversion
+from gtts import gTTS
+
+# This module is imported so that we can
+# play the converted audio
+from playsound import playsound
+
+# These modules are imported to connect to/parse user email
 import imaplib
 import email
+
 mail = imaplib.IMAP4_SSL('imap.gmail.com')
-mail.login(input('email address: '), input('password: '))
+mail.login('gloryadedayo10@gmail.com', 'inner.peace9')
 mail.list()
 mail.select('inbox')
 
 result, data = mail.uid('search', None, "ALL")
 i = len(data[0].split())
+j = 1
 
 email_uid = data[0].split()[i - 7]
 result, email_data = mail.uid('fetch', email_uid, '(RFC822)')
 raw_email = email_data[0][1]
 raw_email_string = raw_email.decode('utf-8')
-
-# converts byte literal to string removing b''
 email_message = email.message_from_string(raw_email_string)
 
-# this will loop through all the available multiparts in mail
 print('Parsing email...\n')
+# this will loop through all the available multiparts in mail
 for part in email_message.walk():
     if part.get_content_type() == "text/plain":
         # ignore attachments/html
         body = part.get_payload(decode=True)
-        print(body.decode('utf-8').strip())
+        mytext = body.decode('utf-8').strip()
         
-        # code to connect to google text-to-speech API
+        # currently working on the code that goes here. this part 
+        # filters the parsed text of unnecessary details like external 
+        # link addresses/long URLs and repeated visual symbols.
+        
+        # lauguage is 'en' (English)
+        myobj = gTTS(text=mytext, lang='en', slow=False)
+
+        # Saving the converted audio in a mp3 file
+        myobj.save("audio.mp3")
+
+        print('Reading email...\n')
+        # play recorded file
+        playsound("C:\\Users\\glory\\Downloads\\code3\\audio.mp3")
 
     else:
         continue
